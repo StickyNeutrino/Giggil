6c65a7f8eecedec5d4a9e2167f6acd3a28f94b52
Daniel Fitchmun <dfitc001@ucr.edu>
2020-08-17 18:07:00 -0700

    Started test cases

---
+++ GiggilTests/GiggilMessageTests.swift
@@ -0,0 +1,26 @@
+//
+//  GiggilMessageTests.swift
+//  GiggilTests
+//
+//  Created by Daniel Fitchmun on 8/17/20.
+//  Copyright © 2020 Fitchmun. All rights reserved.
+//
+import XCTest
+import Foundation
+import Giggil
+
+extension GiggilTests {
+    func testSigningPreservesID() {
+        
+    }
+    
+    func testVerifyReturnsTrueWithPropperKey() {
+
+    }
+    
+    func testVerifyReturnsFalseWithImpropperKey() {
+
+    }
+    
+    
+}
